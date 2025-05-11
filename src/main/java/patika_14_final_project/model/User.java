e9b23eee10859a25830deedfaf76e1502326908d
tvs51 <mustafa_tavusbay@outlook.com>
2025-05-11 16:17:20 +0300

    Added User table,User and User Enums

---
+++ src/main/java/patika_14_final_project/model/User.java
@@ -0,0 +1,73 @@
+package patika_14_final_project.model;
+
+import patika_14_final_project.model.enums.Role;
+
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Role role;
+
+    private  boolean active;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", active=" + active +
+                '}';
+    }
+}
---
+++ src/main/java/patika_14_final_project/model/enums/Role.java
@@ -0,0 +1,8 @@
+package patika_14_final_project.model.enums;
+
+public enum Role {
+
+    ADMIN,
+
+    SUPPORT
+}
